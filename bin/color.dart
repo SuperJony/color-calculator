3fc5e2f3f222375f9390a861be12ea62ccb8b699
Jony <mrsunny125@gmail.com>
2020-08-27 14:12:28 +0800

    adjust cdarkColor's luma

--- bin/color.dart
+++ bin/color.dart
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-08-18 10:13:43
- * @LastEditTime: 2020-08-18 15:21:54
+ * @LastEditTime: 2020-08-27 14:10:07
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /color/bin/color.dart
@@ -10,9 +10,9 @@
 import 'package:color_models/color_models.dart';
 
 void main() {
-  var r = 76;
-  var g = 148;
-  var b = 112;
+  var r = 97;
+  var g = 77;
+  var b = 190;
 
   var calc = CalculatorColor(r: r, g: g, b: b);
   print(calc.darkColor());
@@ -38,12 +38,13 @@ class CalculatorColor {
 
   //设置深色
   List darkColor() {
+    num lumaDark = 0.32;
     var rgbColorValue = <num>[r, g, b];
     var hsvColorValue = <num>[];
 
     _luma = num.parse(luma());
 
-    if (_luma > 0.4) {
+    if (_luma > lumaDark) {
       _rgbColor = RgbColor.fromList(rgbColorValue);
 
       //将 rgb 转成 hsv
@@ -64,11 +65,11 @@ class CalculatorColor {
               0.0722 * rgbColorValue[2]) /
           255;
 
-      //若新色值的 luma 大于 0.4 则降低 luma
-      if (_luma > 0.4) {
-        rgbColorValue[0] = (rgbColorValue[0] * 0.4 / _luma).round();
-        rgbColorValue[1] = (rgbColorValue[1] * 0.4 / _luma).round();
-        rgbColorValue[2] = (rgbColorValue[2] * 0.4 / _luma).round();
+      //若新色值的 luma 大于 lumaDark 则降低 luma
+      if (_luma > lumaDark) {
+        rgbColorValue[0] = (rgbColorValue[0] * lumaDark / _luma).round();
+        rgbColorValue[1] = (rgbColorValue[1] * lumaDark / _luma).round();
+        rgbColorValue[2] = (rgbColorValue[2] * lumaDark / _luma).round();
         return rgbColorValue;
       } else {
         return rgbColorValue;
@@ -79,19 +80,21 @@ class CalculatorColor {
 
   //设置浅色
   List lightColor() {
+    num lumaLight = 0.88;
     var rgbColorValue = <num>[r, g, b];
     var hsvColorValue = <num>[];
 
     //计算颜色亮度 luma
     _luma = num.parse(luma());
 
-    if (_luma < 0.88) {
+    if (_luma < lumaLight) {
       _rgbColor = RgbColor.fromList(rgbColorValue);
 
       //将 rgb 转成 hsv
       _hsvColor = HsvColor.from(_rgbColor);
       hsvColorValue = _hsvColor.toList();
 
+      hsvColorValue[2] = _hsvColor.isBlack ? lumaLight * 100 : hsvColorValue[2];
       //判断 s 是否大于 12，若大于则将 s 设为 12
       hsvColorValue[1] = hsvColorValue[1] > 12.0 ? 12.0 : hsvColorValue[1];
       _hsvColor = HsvColor.fromList(hsvColorValue);
@@ -107,10 +110,10 @@ class CalculatorColor {
           255;
 
       //若新色值的 luma 小于 0.88 则提高 luma
-      if (_luma < 0.88) {
-        rgbColorValue[0] = (rgbColorValue[0] * 0.88 / _luma).round();
-        rgbColorValue[1] = (rgbColorValue[1] * 0.88 / _luma).round();
-        rgbColorValue[2] = (rgbColorValue[2] * 0.88 / _luma).round();
+      if (_luma > 0 && _luma < lumaLight) {
+        rgbColorValue[0] = (rgbColorValue[0] * lumaLight / _luma).round();
+        rgbColorValue[1] = (rgbColorValue[1] * lumaLight / _luma).round();
+        rgbColorValue[2] = (rgbColorValue[2] * lumaLight / _luma).round();
         return rgbColorValue;
       } else {
         return rgbColorValue;
